940faf20f8e59f35620ba3f37145de0f9c476e3c
Ronald Dijks <ronalddijks@protonmail.com>
2021-10-10 00:37:52 +0200

    add vec3 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use vec3::Vec3;
+
+mod vec3;
+
+fn write_color(Vec3(r, g, b): &Vec3) {
+    let ir = (255.999 * r) as u8;
+    let ig = (255.999 * g) as u8;
+    let ib = (255.999 * b) as u8;
+    println!("{} {} {}", ir, ig, ib);
+}
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
@@ -9,11 +20,8 @@ fn main() {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
             let b = 0.25;
-
-            let ir = (255.999 * r) as u8;
-            let ig = (255.999 * g) as u8;
-            let ib = (255.999 * b) as u8;
-            println!("{} {} {}", ir, ig, ib);
+            let color = Vec3(r, g, b);
+            write_color(&color);
         }
     }
     eprint!("\nDone\n");
---
+++ src/vec3.rs
@@ -0,0 +1,199 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Vec3(pub f64, pub f64, pub f64);
+
+impl Vec3 {
+    pub fn zero() -> Self {
+        Self(0.0, 0.0, 0.0)
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.dot(self)
+    }
+
+    pub fn dot(&self, rhs: &Vec3) -> f64 {
+        self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+    }
+
+    pub fn cross(&self, rhs: &Vec3) -> Vec3 {
+        Self(
+            self.1 * rhs.2 - self.2 * rhs.1,
+            self.2 * rhs.0 - self.0 * rhs.2,
+            self.0 * rhs.1 - self.1 * rhs.0,
+        )
+    }
+
+    pub fn unit(&self) -> Vec3 {
+        self / self.length()
+    }
+}
+
+macro_rules! impl_binary_operation {
+    ($operation:ident $op_fun: ident $op_symbol:tt) => {
+        // &Vec `op` &Vec
+        impl<'a, 'b> $operation<&'a Vec3> for &'b Vec3 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: &'a Vec3) -> Vec3 {
+                Vec3(
+                    self.0 $op_symbol other.0,
+                    self.1 $op_symbol other.1,
+                    self.2 $op_symbol other.2
+                )
+            }
+        }
+
+        // Vec `op` &Vec
+        impl<'a> $operation<&'a Vec3> for Vec3 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: &'a Vec3) -> Vec3 {
+                Vec3(
+                    self.0 $op_symbol other.0,
+                    self.1 $op_symbol other.1,
+                    self.2 $op_symbol other.2
+                )
+            }
+        }
+        // &Vec `op` Vec
+        impl<'a> $operation<Vec3> for &'a Vec3 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: Vec3) -> Vec3 {
+                Vec3(
+                    self.0 $op_symbol other.0,
+                    self.1 $op_symbol other.1,
+                    self.2 $op_symbol other.2
+                )
+            }
+        }
+
+        // Vec `op` Vec
+        impl $operation<Vec3> for Vec3 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: Vec3) -> Vec3 {
+                Vec3(
+                    self.0 $op_symbol other.0,
+                    self.1 $op_symbol other.1,
+                    self.2 $op_symbol other.2
+                )
+            }
+        }
+
+        // &Vec3 `op` f64
+        impl <'a> $operation<f64> for &'a Vec3 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: f64) -> Vec3 {
+                Vec3(
+                    self.0 $op_symbol other,
+                    self.1 $op_symbol other,
+                    self.2 $op_symbol other,
+                )
+            }
+        }
+
+        // Vec3 `op` f64
+        impl <'a> $operation<f64> for Vec3 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: f64) -> Vec3 {
+                &self $op_symbol other
+            }
+        }
+
+        // f64 `op` Vec3
+        impl <'a> $operation<Vec3> for f64 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: Vec3) -> Vec3 {
+                &other $op_symbol self
+            }
+        }
+
+        // f64 `op` Vec3
+        impl <'a> $operation<&'a Vec3> for f64 {
+            type Output = Vec3;
+
+            #[inline]
+            fn $op_fun(self, other: &'a Vec3) -> Vec3 {
+                other $op_symbol self
+            }
+        }
+    };
+}
+
+macro_rules! impl_unary_operation {
+
+    ($operation:ident $op_fun:ident $op_symbol:tt) => {
+      impl<'a> $operation for &'a Vec3 {
+        type Output = Vec3;
+
+        #[inline]
+        fn $op_fun(self) -> Vec3 {
+          Vec3 (
+            $op_symbol self.0,
+            $op_symbol self.1,
+            $op_symbol self.2,
+          )
+        }
+      }
+
+      // Have the operator on values forward through to the implementation
+      // above
+      impl $operation for Vec3 {
+        type Output = Vec3;
+
+        #[inline]
+        fn $op_fun(self) -> Vec3 {
+          $op_symbol &self
+        }
+      }
+    };
+}
+
+macro_rules! impl_op_assign {
+    ($op_type:ident $op_fun:ident $op_symbol:tt) => {
+      impl<'a> $op_type<&'a Vec3> for Vec3 {
+        fn $op_fun(&mut self, other: &'a Vec3) {
+          *self = Vec3 (
+            self.0 $op_symbol other.0,
+            self.0 $op_symbol other.0,
+            self.0 $op_symbol other.0,
+          );
+        }
+      }
+
+      impl $op_type for Vec3 {
+        #[inline]
+        fn $op_fun(&mut self, other: Vec3) {
+          *self = *self $op_symbol &other
+        }
+      }
+    };
+  }
+
+impl_binary_operation!(Add add +);
+impl_binary_operation!(Sub sub -);
+impl_binary_operation!(Mul mul *);
+impl_binary_operation!(Div div /);
+
+impl_op_assign!(AddAssign add_assign +);
+impl_op_assign!(SubAssign sub_assign -);
+impl_op_assign!(MulAssign mul_assign *);
+impl_op_assign!(DivAssign div_assign /);
+
+impl_unary_operation!(Neg neg -);
