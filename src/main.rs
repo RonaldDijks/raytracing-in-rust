f17d5cbf4faecad79d30a3602ab7d2ef84109f39
Ronald Dijks <ronalddijks@protonmail.com>
2021-10-10 22:50:22 +0200

    draw sphere normals

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,25 @@ fn write_color(Vec3(r, g, b): &Vec3) {
     println!("{} {} {}", ir, ig, ib);
 }
 
-fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin - center;
     let a = ray.direction.dot(&ray.direction);
     let b = 2.0 * oc.dot(&ray.direction);
     let c = oc.dot(&oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(ray: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3(0., 0., -1.), 0.5, ray) {
-        return Vec3(1., 0., 0.);
+    let t = hit_sphere(&Vec3(0., 0., -1.), 0.5, ray);
+    if t > 0.0 {
+        let normal = (ray.at(t) - Vec3(0.0, 0.0, -1.0)).unit();
+        return 0.5 * Vec3(normal.0 + 1.0, normal.1 + 1.0, normal.2 + 1.0);
     }
     let t = 0.5 * (ray.direction.unit().1 + 1.0);
     (1.0 - t) * Vec3::one() + t * Vec3(0.5, 0.7, 1.0)
@@ -31,7 +38,7 @@ fn ray_color(ray: &Ray) -> Vec3 {
 fn main() {
     // image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
 
     // camera
