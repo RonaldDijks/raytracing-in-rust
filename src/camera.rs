19d49a61d0ac9d5470ef9b7580deb678e295beb9
Ronald Dijks <ronalddijks@protonmail.com>
2021-10-11 00:20:48 +0200

    add anti aliasing

---
+++ src/camera.rs
@@ -0,0 +1,41 @@
+use crate::{ray::Ray, vec3::Vec3};
+
+pub struct Camera {
+    pub origin: Vec3,
+    pub lower_left_corner: Vec3,
+    pub horizontal: Vec3,
+    pub vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new(origin: Vec3, viewport_width: f64, viewport_height: f64, focal_length: f64) -> Self {
+        let horizontal = Vec3(viewport_width, 0.0, 0.0);
+        let vertical = Vec3(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
+
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical,
+        )
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+        Camera::new(Vec3::zero(), viewport_width, viewport_height, focal_length)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use hittable::*;
-use ray::Ray;
-use vec3::Vec3;
-
+mod camera;
 mod hittable;
 mod ray;
 mod vec3;
 
-fn write_color(Vec3(r, g, b): &Vec3) {
-    let ir = (255.999 * r) as u8;
-    let ig = (255.999 * g) as u8;
-    let ib = (255.999 * b) as u8;
-    println!("{} {} {}", ir, ig, ib);
+use camera::Camera;
+use hittable::*;
+use rand::Rng;
+use ray::Ray;
+use vec3::Vec3;
+
+fn write_color(pixel_color: &Vec3, samples_per_pixel: u32) {
+    let scale = 1.0 / samples_per_pixel as f64;
+    let Vec3(r, g, b) = pixel_color * scale;
+    let r = (r.clamp(0.0, 0.999) * 256.0) as u8;
+    let g = (g.clamp(0.0, 0.999) * 256.0) as u8;
+    let b = (b.clamp(0.0, 0.999) * 256.0) as u8;
+    println!("{} {} {}", r, g, b);
 }
 
 fn ray_color(ray: &Ray, hittable: &dyn Hittable) -> Vec3 {
@@ -27,6 +32,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // world
     let world = HittableList::new(vec![
@@ -35,27 +41,23 @@ fn main() {
     ]);
 
     // camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
+    let camera = Camera::default();
 
-    let origin = Vec3::zero();
-    let horizontal = Vec3(viewport_width, 0.0, 0.0);
-    let vertical = Vec3(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
+    let mut rng = rand::thread_rng();
 
     // render
     print!("P3\n{} {}\n255\n", image_width, image_height);
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {}     ", j);
         for i in 0..image_width {
-            let u = (i as f64) / ((image_width - 1) as f64);
-            let v = (j as f64) / ((image_height - 1) as f64);
-            let direction = lower_left_corner + u * horizontal + v * vertical - origin;
-            let r = Ray::new(origin, direction);
-            let color = ray_color(&r, &world);
-            write_color(&color);
+            let mut pixel_color = Vec3::zero();
+            for _ in 0..samples_per_pixel {
+                let u = (i as f64 + rng.gen::<f64>()) / (image_width - 1) as f64;
+                let v = (j as f64 + rng.gen::<f64>()) / (image_height - 1) as f64;
+                let ray = camera.get_ray(u, v);
+                pixel_color += ray_color(&ray, &world);
+            }
+            write_color(&pixel_color, samples_per_pixel);
         }
     }
     eprint!("\nDone\n");
